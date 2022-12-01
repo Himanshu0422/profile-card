ea1f641dbc9952a837f007fa3b767e8c9e134799
Himanshu0422 <himanshumittal035@gmail.com>
2022-12-01 10:56:02 +0530

    unnecessary code delete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
 import Card from "./components/Card";
-// import Header from "../components/Header";
 import Main from "./components/Main";
-// import Footer from "../components/Footer";
-// import Logo from "../components/Logo";
 
 export default function App(){
     return (
